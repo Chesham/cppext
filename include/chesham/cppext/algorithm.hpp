20798f2a31b526e0fc9239205b55d3ef64918753
Chesham <c.moweb@gmail.com>
2020-05-19 23:33:46 +0800

    放寬序列迭代比較對象，現在指標也被支援

--- include/chesham/cppext/algorithm.hpp
+++ include/chesham/cppext/algorithm.hpp
@@ -7,7 +7,7 @@ namespace chesham
     namespace cppext
     {
         template<typename It>
-        typename std::enable_if<std::is_base_of<std::forward_iterator_tag, typename It::iterator_category>::value, bool>::type sequence_equal(It first1, It end1, It first2, It end2)
+        bool sequence_equal(It first1, It end1, It first2, It end2)
         {
             while (first1 != end1 && first2 != end2)
             {
